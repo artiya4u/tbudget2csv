e79f6e71d695e19e4b2023829bb130669cf0c804
Artiya Thinkumpang <artiya4u@gmail.com>
2020-12-10 18:59:46 +0700

    Init

---
+++ README.md
@@ -0,0 +1,10 @@
+# TBudget2CSV
+A tool to convert Thai National budget PDF to machine readable CSV format.
+
+## Getting start
+- Install pdf2text on Linux `sudo apt install python3-pdfminer`
+
+## Usage
+`python3 tbudget2csv.py budget2021.pdf`
+
+ 
---
+++ tbudget2csv.py
@@ -0,0 +1,103 @@
+import csv
+import os
+import sys
+import re
+
+specials = ['ุ', 'ู', 'ึ', 'ำ', 'ั', 'ี', '้', '็', '่', '๋']
+
+
+def fix_align(txt):
+    input_txt = list(txt)
+    result = list('')
+    skip_next = False
+    for idx, val in enumerate(input_txt):
+        if val in specials and idx > 1 and idx + 1 < len(input_txt) and input_txt[idx + 1] == ' ':
+            temp = result.pop()
+            result.append(val)
+            result.append(temp)
+            skip_next = True
+        else:
+            if not skip_next:
+                result.append(val)
+            skip_next = False
+
+    return ''.join(result)
+
+
+def thai_number_to_arabic(thai_number):
+    thai_number = thai_number.replace('๐', '0')
+    thai_number = thai_number.replace('๑', '1')
+    thai_number = thai_number.replace('๒', '2')
+    thai_number = thai_number.replace('๓', '3')
+    thai_number = thai_number.replace('๔', '4')
+    thai_number = thai_number.replace('๕', '5')
+    thai_number = thai_number.replace('๖', '6')
+    thai_number = thai_number.replace('๗', '7')
+    thai_number = thai_number.replace('๘', '8')
+    thai_number = thai_number.replace('๙', '9')
+    thai_number = thai_number.replace(',', '')
+    return thai_number
+
+
+if __name__ == '__main__':
+    os.system(f'pdftotext {sys.argv[1]} -layout')
+    text_file_name = sys.argv[1].replace('.pdf', '.txt')
+    records = []
+    with open(text_file_name) as text_file:
+        lines = text_file.readlines()
+        budget_name = ''
+        is_record = False
+        section = ''
+        organization = ''
+        is_org = False
+        for line in lines:
+            line_strip_arabic = thai_number_to_arabic(line.strip())
+            if line_strip_arabic.startswith('มาตรา'):
+                section = line_strip_arabic[0:line_strip_arabic.find(' ', 6)]
+
+            if re.match("\d+\.", line_strip_arabic):
+                organization = ''
+                is_org = True
+
+            if is_org:
+                organization += line_strip_arabic
+
+            if line_strip_arabic.endswith('บาท คือ') or line_strip_arabic.endswith('ประกอบด้วย'):
+                is_org = False
+
+            if re.match("\(.+\)", line_strip_arabic):
+                budget_name = ''
+                is_record = True
+
+            if line_strip_arabic.endswith('บาท'):
+                amount_strip = line_strip_arabic.replace(' บาท', '')
+                amount = re.findall("\d+", amount_strip)[-1]
+                budget_name += amount_strip.replace(amount, '').strip()
+                budget_name = budget_name[budget_name.find(' ', 0) + 1:]
+
+                find_end = organization.find(' รวม')
+                if find_end == -1:
+                    find_end = organization.find('ให้')
+
+                organizationStrip = organization[organization.find(' ', 0):find_end].strip().replace(' ', '')
+                b = {
+                    'section': section,
+                    'organization': organizationStrip,
+                    'name': fix_align(budget_name),
+                    'amount': amount
+                }
+                budget_name = ''
+                if is_record:
+                    print(b)
+                    records.append(b)
+                is_record = False
+
+            if is_record and not is_org:
+                budget_name += line_strip_arabic
+
+        if len(records) > 0:
+            csv_file_name = sys.argv[1].replace('.pdf', '.csv')
+            f = open(csv_file_name, 'w')
+            w = csv.DictWriter(f, records[0].keys())
+            w.writerows(records)
+            f.close()
