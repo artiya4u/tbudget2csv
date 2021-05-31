bb4c06f992716bcf0a451ee7e68fb93f1c392f54
Artiya Thinkumpang <artiya4u@gmail.com>
2021-06-01 00:44:42 +0700

    Fix some case.

--- tbudget2csv_red.py
+++ tbudget2csv_red.py
@@ -6,7 +6,12 @@ import re
 from fixtext import fix_align
 
 project_title_prefix = ('ผลผลิต', 'แผนงาน', 'โครงการ')
-org_prefix = ((' ' * 30) + 'กระทรวง', (' ' * 30) + 'สานักนายก', (' ' * 30) + 'องค์กรปกครอง')
+org_prefix = [(' ' * 30) + 'กระทรวง', (' ' * 30) + 'สานักนายก', (' ' * 30) + 'องค์กรปกครอง', (' ' * 30) + 'จังหวัดและก']
+section_6_2_prefix = [
+    '6.2 จาแนกตามแผนงาน ผลผลิต/โครงการ และงบรายจ่าย',
+    '6.2 จําแนกตามแผนงาน ผลผลิต/โครงการ และงบรายจ่าย',
+    '6. สรุปงบประมาณรายจ่ายประจาปี งบประมาณ'
+]
 
 
 def replace_dash(text):
@@ -16,15 +21,15 @@ def replace_dash(text):
         return text
 
 
-def convert_6_2_table(pdf_budget_file):
+def convert_table_6(pdf_budget_file):
     print(f'Start convert: {pdf_budget_file}')
-    # os.system(f'pdftotext -layout {pdf_budget_file}')
+    os.system(f'pdftotext -layout {pdf_budget_file}')
     text_file_name = pdf_budget_file.replace('.pdf', '.txt')
     project_budgets = []
     with open(text_file_name) as text_file:
         count = 0
         lines = text_file.readlines()
-        is_section_6_2 = False
+        is_section_6 = False
         is_row = False
         org_name = None
         sub_org_name = None
@@ -48,22 +53,21 @@ def convert_6_2_table(pdf_budget_file):
             segments = list(map(replace_dash, segments))
 
             # Condition find for section 6.2
-            if line.find('6.2 จาแนกตามแผนงาน ผลผลิต/โครงการ และงบรายจ่าย') == 0 \
-                    or line.find('6. สรุปงบประมาณรายจ่ายประจาปี งบประมาณ ') == 0:
-                is_section_6_2 = True
+            if any(line.startswith(x) for x in section_6_2_prefix):
+                is_section_6 = True
                 continue
 
             # Inside 6.2 section loop fill all value
-            if line.startswith('รวม') and is_section_6_2:
+            if line.startswith('รวม') and is_section_6:
                 is_row = True
                 continue
 
-            if is_section_6_2 and is_row:
+            if is_section_6 and is_row:
                 print(segments)
                 no_number_title = re.sub(r'\d\.', '', segments[0]).strip()
                 if no_number_title.startswith(project_title_prefix) \
                         or segments[0].find('7. รายละเอียดงบประมาณจ') >= 0:
-                    if project_name != '' and sum_budget is not None:
+                    if project_name != '' and sum_budget is not None and sum_budget != 'รวม':
                         is_plan = re.search(r'\d\.', project_name) is not None
                         plan = {
                             'org_name': org_name,
@@ -84,7 +88,7 @@ def convert_6_2_table(pdf_budget_file):
 
                 if segments[0].find('7. รายละเอียดงบประมาณจ') >= 0:
                     is_row = False
-                    is_section_6_2 = False
+                    is_section_6 = False
                     sum_budget = None
                     continue
 
@@ -101,8 +105,12 @@ def convert_6_2_table(pdf_budget_file):
                     sum_budget = segments[6]
 
     if len(project_budgets) > 0:
-        csv_file_name = 'budget_by_project.csv'
-        f = open(csv_file_name, 'a')
+        try:
+            os.makedirs('output/')
+        except OSError:
+            pass
+        csv_file_name = 'output/' + pdf_budget_file.split('/')[1].replace('.pdf', '.csv')
+        f = open(csv_file_name, 'w')
         w = csv.DictWriter(f, project_budgets[0].keys())
         w.writerows(project_budgets)
         f.close()
@@ -113,4 +121,4 @@ if __name__ == '__main__':
     list_of_files = sorted(filter(os.path.isfile, glob.glob(pdf_path + '*.pdf')))
     for file in list_of_files:
         if file.endswith('.pdf'):
-            convert_6_2_table(file)
+            convert_table_6(file)
